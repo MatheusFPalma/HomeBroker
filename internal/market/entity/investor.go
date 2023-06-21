a4621c179f6327e00a4ec0ea292f64be66102689
MatheusFPalma <matheusfpalma@hotmail.com>
2023-06-21 14:15:23 -0300

    Investor2

--- internal/market/entity/investor.go
+++ internal/market/entity/investor.go
@@ -6,6 +6,13 @@ type Investor struct {
 	AssetPosition []*InvestorAssetPosition
 }
 
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:            id,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
 type InvestorAssetPosition struct {
 	AssetID string
 	Shares  int
